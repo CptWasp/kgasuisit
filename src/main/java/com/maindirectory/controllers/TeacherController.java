e3684b4b450d6570cd7103205bb1fdeb32e515ff
CptWasp <stignelson11@gmail.com>
2020-10-30 20:10:01 +0300

    updating teachers datas

--- src/main/java/com/maindirectory/controllers/TeacherController.java
+++ src/main/java/com/maindirectory/controllers/TeacherController.java
@@ -71,6 +71,44 @@ public class TeacherController {
         return "teachersuploading";
     }
 
+    @PostMapping("/teacherupdate")
+    public String updateTeachersData(@RequestParam String num, Map<String, Object> model){
+
+        System.out.println(num);
+
+        JSONParser parser = new JSONParser();
+
+        try {
+            JSONArray a = (JSONArray) parser.parse(new FileReader("./jsons/notesJson"+num+".json"));
+
+            for (Object o : a)
+            {
+                JSONObject person = (JSONObject) o;
+
+                String name = (String) person.get("name");
+                System.out.println(name);
+
+                String link = (String) person.get("link");
+                System.out.println(link);
+
+                Teacher teacher = new Teacher(name, link, 0.0);
+                teacherRepo.save(teacher);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Iterable<Teacher> teachers = teacherRepo.findAll();
+        model.put("teachers", teachers);
+
+        return "teachersuploading";
+    }
 
 
 
